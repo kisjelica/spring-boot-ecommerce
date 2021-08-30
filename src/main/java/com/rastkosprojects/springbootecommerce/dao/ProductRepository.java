135158a6dbca5fa0c79c078292dbde39dc598323
Kisjelica <kisjelicarastko@gmail.com>
2021-08-30 18:01:27 +0200

    Added cross-origin support.

--- src/main/java/com/rastkosprojects/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/com/rastkosprojects/springbootecommerce/dao/ProductRepository.java
@@ -7,11 +7,13 @@ package com.rastkosprojects.springbootecommerce.dao;
 
 import com.rastkosprojects.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  *
  * @author rastko
  */
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
     
 }
