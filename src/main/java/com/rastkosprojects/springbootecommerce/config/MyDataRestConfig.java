59e1f5160e9626e3ea21fd37570a225a2c74ea67
Kisjelica <kisjelicarastko@gmail.com>
2021-08-31 03:59:47 +0200

    Added support for dynamic search by category.

--- src/main/java/com/rastkosprojects/springbootecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/rastkosprojects/springbootecommerce/config/MyDataRestConfig.java
@@ -7,6 +7,12 @@ package com.rastkosprojects.springbootecommerce.config;
 
 import com.rastkosprojects.springbootecommerce.entity.Product;
 import com.rastkosprojects.springbootecommerce.entity.ProductCategory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -20,6 +26,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    
+    
+    
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
@@ -32,6 +47,24 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         config.getExposureConfiguration().forDomainType(ProductCategory.class)
         .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
         .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+        
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        //expose entity ids
+        
+        //get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        
+        List<Class> entityClasses = new ArrayList<>();
+        //get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+        //expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
         
     }
 
--- src/main/java/com/rastkosprojects/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/com/rastkosprojects/springbootecommerce/dao/ProductRepository.java
@@ -6,8 +6,11 @@
 package com.rastkosprojects.springbootecommerce.dao;
 
 import com.rastkosprojects.springbootecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -15,5 +18,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
  */
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
-    
+    /*Query method because it starts with find by.
+      Matches by category id.
+      Behind the scenes, Spring executes a statement like:
+      SELECT * FROM product WHERE category_id=?
+      Spring Data REST will also expose the endpoint for:
+      http://localhost:8080/api/products/search/findByCategory?id=2
+    */
+    Page<Product> findByCategoryId(@RequestParam("id")Long id, Pageable pageable);
 }
